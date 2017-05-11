eda1f64063a2f085d4ba1f2047dca0c3b97af48f
Nicolas BRU <nicolasbru.dev@gmail.com>
2017-05-11 14:47:17 +0200

    Init Sudoku solver

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'app');
+
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    watch: true
+};
+
+module.exports = config;
